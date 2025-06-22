da280e8e9bc05af71d6d0bd6bacc5691770ef51d
Manan Bhateja <bhateja.manan@gmail.com>
2025-06-22 20:08:38 +0530

    Watermark

--- src/components/Watermark.tsx
+++ src/components/Watermark.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Watermark = () => {
   return (
     <div className="fixed bottom-4 right-4 z-10 pointer-events-none select-none">
-      <span className="text-xs text-gray-400/60 font-light tracking-wide">
+      <span className="text-xs text-black-500/60 font-light tracking-wide">
         Team CodeCraft 2025
       </span>
     </div>
